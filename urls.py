2946be2b1e6c4df8452935d59c4af60b5147701f
Vinícius das Chagas Silva <vinimaster@gmail.com>
2010-10-31 11:24:50 -0200

    adicionadas urls da app de atividades

--- urls.py
+++ urls.py
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     (r'^programacao/', include('pythoncampusiff_programacao.urls')),
     (r'^inscricao/', include('pythoncampusiff_inscricao.urls')),
     (r'^contato/', include('pythoncampusiff_contato.urls')),
+    (r'^atividades/', include('pythoncampusiff_atividades.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'^site_media/(.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}
